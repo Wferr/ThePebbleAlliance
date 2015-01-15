2eb86d1d66956171989a8b9aec2df29add074699
Wferr <Wferr@users.noreply.github.com>
2015-01-15 15:42:49 -0800

    Working :D

--- src/app.js
+++ src/app.js
@@ -1,7 +1,14 @@
 // Requires
 var UI = require('ui');
 var ajax = require('ajax');
+//var Settings = require('settings');
+var teamNumber = ('2102');
 
+
+//Setting Team Number
+//Settings.config({
+//  Settings.option(teamNumber, "2102");
+//});
 // Static Cards
 var splashWindow = new UI.Window({
   title:'Fetching Data',
@@ -12,26 +19,38 @@ var splashWindow = new UI.Window({
 // Display Splash Card
 splashWindow.show();
 
-//r teamNumber = 'frc' + '2102';
-var API ='www.thebluealliance.com/api/v2/team/frc2102';
+var apiId ="?X-TBA-App-Id=team2102:pebble-app:v01";
+var API ='http://www.thebluealliance.com/api/v2/team/frc'+ teamNumber + apiId;
 
 ajax(
   {
     url: API,
     type: 'json',
-    headers: { 'X-TBA-App-Id' : "frc2102:pebble-app:v01"}},
+   // headers: { 'X-TBA-App-Id' : "frc2102:pebble-app:v01"}
+  },
   function(data) {
     console.log("Grabbed data");
-    console.log('Grabbed data!' + data);
-    var teamInfo = data.name;
-    var teamInfoCard= new UI.Card({
-      title:'Team Info',
-      subtitle:teamInfo,
-    });
-    teamInfoCard.show();
+    var teamInfoMenu= new UI.Menu({
+  sections: [{
+    title: 'Team Info',
+    items: [{
+      title: 'Team Name',
+      subtitle: data.nickname 
+    }, {
+      title: 'Team Number',
+      subtitle: data.team_number
+    }, {
+      title: 'Rookie Year',
+      subtitle: data.rookie_year
+    }, {
+      title: 'Team Location',
+      subtitle: data.location
+    }]
+  }]
+});
+    teamInfoMenu.show();
+    splashWindow.hide();
     },
   function(error) {
-    console.log('Failed:' + error);
     console.log("Error");
-  }
-);
+  });
