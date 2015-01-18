f462ca4858f0d61c0fe41a33ae6edce445728aa4
Wferr <Wferr@users.noreply.github.com>
2015-01-17 18:42:38 -0800

    Shake to Update

--- src/app.js
+++ src/app.js
@@ -7,6 +7,9 @@ var Settings = require('settings');
 var Vibe = require('ui/vibe');
 
 var teamNumber = '2102';
+var apiId ="?X-TBA-App-Id=team2102:pebble-app:v01";
+var API ='http://www.thebluealliance.com/api/v2/team/frc'+ teamNumber + apiId;
+
 //Setting Team Number
 Settings.config(
   { url:"http://wferr.com/ThePebbleAlliance/"}
@@ -15,44 +18,38 @@ Settings.option({
   teamNumber: '2102',
 });
 
-
-
-
-
 // Static Cards
 
+//Splash
+//Create Splash Window
 var splashWindow = new UI.Window({ fullscreen: true});
+
+//Create Splash Logo
 var splashLogo = new UI.Image ({
   posisiton: new Vector2 (0,0),
   size: new Vector2 (144,168),
   image: "images/logosplash.png"
 });
+//Add Splash Logo to Splash Window
 splashWindow.add(splashLogo);
 
+//Updated Window Card
 var updatedWindow = new UI.Card({
   title: 'Data Updated',
   subtitle: 'You are upto Date'
 });
 
+//End Static Cards
 
-
-
-// Display Splash Card
-splashWindow.show();
-
-Accel.init();
-
-var apiId ="?X-TBA-App-Id=team2102:pebble-app:v01";
-var API ='http://www.thebluealliance.com/api/v2/team/frc'+ teamNumber + apiId;
-
+//Update Team Info Function
+var updateTeamInfo = function(){
 ajax(
   {
     url: API,
     type: 'json',
-   // headers: { 'X-TBA-App-Id' : "frc2102:pebble-app:v01"}
   },
   function(data) {
-    console.log("Grabbed data");
+    console.log("Grabbed Team Info Page");
     var teamInfoMenu= new UI.Menu({
   sections: [{
     title: 'Team Info',
@@ -77,5 +74,22 @@ ajax(
     updatedWindow.show();
     },
   function(error) {
-    console.log("Error");
+    console.log("Error Fetching Team Info Page");
   });
+};
+
+
+
+
+// Display Splash Card
+splashWindow.show();
+updateTeamInfo();
+
+Accel.init();
+var updateData = function(){
+  updateTeamInfo();
+  setTimeout(updatedWindow.show(), 500);
+  updatedWindow.hide();
+  
+};
+Accel.on('tap', updateData);
